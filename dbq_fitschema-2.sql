d4da8b86021f1cd3f83f769b953f3af3d4e3fc4d
edmyomet <shrut828@gmail.com>
2024-06-19 14:03:27 +0530

    Modifications
    Figuring out how to access columns via variables in order to load death toll values from the disease columns of dbq_temporary_table

--- dbq_fitschema-2.sql
+++ dbq_fitschema-2.sql
@@ -1,4 +1,49 @@
---entering death tolls for countries during the year 1990
-SELECT disease_name
+--queries to load the structure of various tables 
+--selecting singlular column name from columns returned by information scheme
+SELECT column_name as cname
+FROM INFORMATION_SCHEMA.columns
+WHERE TABLE_NAME = 'dbq_temporary_table'
+AND column_name = 'parkinsons';
+
+--displaying disease ids with their corresponding disease names (displayed as columns)
+--in the dbq_temporary_table relation, using information schema and left join on disease name
+SELECT diseases.disease_id, tmp.cname 
 FROM diseases
-WHERE disease_id=1;
+LEFT OUTER JOIN (
+    SELECT column_name AS cname
+    FROM INFORMATION_SCHEMA.columns
+    WHERE TABLE_NAME='dbq_temporary_table' 
+) AS tmp(cname)
+ON diseases.disease_name = tmp.cname;
+
+--selecting the names of countries and the diseases 
+SELECT countries.country_name, tmp.cname
+FROM countries
+CROSS JOIN (
+    SELECT column_name AS cname 
+    FROM INFORMATION_SCHEMA.columns
+    WHERE TABLE_NAME = 'dbq_temporary_table'
+) AS tmp(cname)
+WHERE tmp.cname NOT IN (
+    VALUES ('country'),('country_code'), ('year'), ('index')
+);
+
+DO $$
+DECLARE column_vars VARCHAR[];
+BEGIN 
+    SELECT column_name INTO column_vars
+    FROM INFORMATION_SCHEMA.columns
+    WHERE TABLE_NAME = 'dbq_temporary_schema'
+    AND column_name NOT IN (
+        VALUES ('country'),('country_code'),('year'),('index')
+    );
+    SELECT ARRAY(column_vars);
+END $$; 
+
+
+
+
+
+
+
+
